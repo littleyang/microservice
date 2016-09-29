c3986e5595d2e932eed1b497f5bd85a8853f5367
littleyang <littleyangqh@gmail.com>
2016-09-29 18:24:55 +0800

    init

---
+++ compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+	}
+
+}
