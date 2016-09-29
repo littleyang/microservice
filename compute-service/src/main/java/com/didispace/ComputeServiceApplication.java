2f416fe9df9f51aa8cc05e8f5b438d46dff1b021
littleyang <littleyangqh@gmail.com>
2016-09-29 18:43:50 +0800

    add micro service

--- compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
+++ compute-service/src/main/java/com/didispace/ComputeServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ComputeServiceApplication {
+public class ComputeServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
--- eureka-server/src/main/java/com/didispace/Application.java
+++ eureka-server/src/main/java/com/didispace/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
