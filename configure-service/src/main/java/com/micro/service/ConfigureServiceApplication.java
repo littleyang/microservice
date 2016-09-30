1be5e36c31403598ca8f657196fd3a221bb44809
littleyang <littleyangqh@gmail.com>
2016-09-30 12:13:05 +0800

    init project register server

--- configure-service/src/main/java/com/micro/service/ConfigureServiceApplication.java
+++ configure-service/src/main/java/com/micro/service/ConfigureServiceApplication.java
@@ -2,8 +2,10 @@ package com.micro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigureServiceApplication {
 
 	public static void main(String[] args) {
