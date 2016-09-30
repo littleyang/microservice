39a56a167ec765d9253634175b30ae585a28b255
littleyang <littleyangqh@gmail.com>
2016-09-30 11:44:28 +0800

    init project

---
+++ configure-service/src/main/java/com/micro/service/ConfigureServiceApplication.java
@@ -0,0 +1,12 @@
+package com.micro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureServiceApplication.class, args);
+	}
+}
