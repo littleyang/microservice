a572e8fdd717860695e068d25e78779582f2d6a7
littleyang <littleyangqh@gmail.com>
2016-10-11 14:00:51 +0800

    add api gate way

--- route-service/src/main/java/com/micro/service/RouteServiceApplication.java
+++ route-service/src/main/java/com/micro/service/RouteServiceApplication.java
@@ -1,12 +1,21 @@
 package com.micro.service;
 
+import com.micro.service.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class RouteServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RouteServiceApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
