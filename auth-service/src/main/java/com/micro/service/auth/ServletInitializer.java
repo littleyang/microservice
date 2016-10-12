a92c93b780af22a51f62936c6e474f2f9fd1a23f
littleyang <littleyangqh@gmail.com>
2016-10-12 10:35:48 +0800

    add auth service

---
+++ auth-service/src/main/java/com/micro/service/auth/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.micro.service.auth;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthServiceApplication.class);
+	}
+
+}
