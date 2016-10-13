825107f064dff87a83e500f2ab98c7ff449d9531
littleyang <littleyangqh@gmail.com>
2016-10-13 14:08:43 +0800

    add oauth

--- auth-service/src/main/java/com/micro/service/auth/config/AlanOAuthWebConfig.java
+++ auth-service/src/main/java/com/micro/service/auth/config/AlanOAuthWebConfig.java
@@ -11,17 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
-/**
- * Created by wanghongfei(hongfei7@staff.sina.com.cn) on 9/12/16.
- */
 @Configuration
 @EnableWebSecurity
-
 public class AlanOAuthWebConfig extends WebSecurityConfigurerAdapter {
 
     @Override
