8e3b1f55f5eefc323faf350617b39fcb0df61f8c
littleyang <littleyangqh@gmail.com>
2016-11-15 18:26:57 +0800

    add coupon project

---
+++ coupon/coupon-api/src/main/java/com/micro/service/coupon/api/CouponApiCccc.java
@@ -0,0 +1,15 @@
+package com.micro.service.coupon.api;
+
+/**
+ * @description:
+ * @author: yang.zhou
+ * @version: 1.0.0
+ * @since: 2016-11-15 6:18 PM
+ */
+
+public class CouponApiCccc {
+
+    public String hello(String username){
+        return "hello, " + username;
+    }
+}
