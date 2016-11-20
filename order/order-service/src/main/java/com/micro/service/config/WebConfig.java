cd40ad0d641420b4d697ed997d5f3075a103dd13
littleyang <littleyangqh@gmail.com>
2016-11-20 09:47:48 +0800

    add register

--- order/order-service/src/main/java/com/micro/service/config/WebConfig.java
+++ order/order-service/src/main/java/com/micro/service/config/WebConfig.java
@@ -1,10 +1,17 @@
 //package com.micro.service.config;
 //
+//import com.fasterxml.jackson.databind.DeserializationFeature;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.http.MediaType;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 //import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//import java.util.List;
 //
 ///**
 // * @description:
@@ -28,5 +35,17 @@
 //                defaultContentType(MediaType.APPLICATION_JSON).
 //                mediaType("xml", MediaType.APPLICATION_XML).
 //                mediaType("json", MediaType.APPLICATION_JSON);
+//    }
+//
+//
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(mappingJackson2HttpMessageConverter());
+//    }
+//
+//    @Bean
+//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+//        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//        converter.setObjectMapper(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+//        return converter;
 //    }
 //}
