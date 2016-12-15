9957995c8d93d9aeb1ed080148e020a646d7e586
littleyang <littleyangqh@gmail.com>
2016-12-15 19:29:17 +0800

    add promo project

--- coupon/coupon-service/src/main/java/com/micro/service/coupon/CouponServiceApplication.java
+++ coupon/coupon-service/src/main/java/com/micro/service/coupon/CouponServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class CouponServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
