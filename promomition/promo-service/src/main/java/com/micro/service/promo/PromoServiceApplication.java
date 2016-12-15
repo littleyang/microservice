8e30d002918392b3a186318de5394b9d794f0446
littleyang <littleyangqh@gmail.com>
2016-12-15 19:32:21 +0800

    add promo project

--- promomition/promo-service/src/main/java/com/micro/service/promo/PromoServiceApplication.java
+++ promomition/promo-service/src/main/java/com/micro/service/promo/PromoServiceApplication.java
@@ -2,9 +2,10 @@ package com.micro.service.promo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class PromoServiceApplication {
 
 	public static void main(String[] args) {
