1742deecd900385a0016a40eabc1dd87a099fd38
littleyang <littleyangqh@gmail.com>
2016-12-15 18:59:55 +0800

    add promomition pro

---
+++ promomition/promo-service/src/main/java/com/micro/service/promo/PromoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.micro.service.promo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoServiceApplication.class, args);
+	}
+}
