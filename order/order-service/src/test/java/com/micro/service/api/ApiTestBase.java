45be0dfd8fa8329fb02e1c1ff2b376ea8d42e725
littleyang <littleyangqh@gmail.com>
2017-01-10 14:34:09 +0800

    add common utils project

--- order/order-service/src/test/java/com/micro/service/api/ApiTestBase.java
+++ order/order-service/src/test/java/com/micro/service/api/ApiTestBase.java
@@ -1,5 +1,8 @@
 package com.micro.service.api;
 
+import com.micro.service.utils.MainUtils;
+import org.junit.Test;
+
 /**
  * @description:
  * @author: yang.zhou
@@ -8,4 +11,10 @@ package com.micro.service.api;
  */
 
 public class ApiTestBase {
+
+    @Test
+    public void testCommonUtilsTest(){
+        MainUtils utils = new MainUtils();
+        System.out.println(utils.sayHello("my world!!!!"));
+    }
 }
