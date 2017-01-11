550cecbb5de9cb1577b310ba252ff6fec5752370
littleyang <littleyangqh@gmail.com>
2017-01-11 17:57:12 +0800

    add api token oauth

--- route-service/src/main/java/com/micro/service/RouteServiceApplication.java
+++ route-service/src/main/java/com/micro/service/RouteServiceApplication.java
@@ -1,16 +1,30 @@
 package com.micro.service;
 
 import com.micro.service.filter.AccessFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @EnableZuulProxy
 @Controller
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableOAuth2Sso
+@EnableResourceServer
+@EnableAuthorizationServer
 public class RouteServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
