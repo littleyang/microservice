35d5495e7e338846fe055590da264cb3c54ae487
littleyang <littleyangqh@gmail.com>
2017-01-11 17:29:25 +0800

    add current user

--- oauth-center/src/main/java/com/micro/service/oauth/controller/rest/user/AuthUserController.java
+++ oauth-center/src/main/java/com/micro/service/oauth/controller/rest/user/AuthUserController.java
@@ -1,5 +1,6 @@
 package com.micro.service.oauth.controller.rest.user;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import java.security.Principal;
  */
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthUserController {
 
     /**
