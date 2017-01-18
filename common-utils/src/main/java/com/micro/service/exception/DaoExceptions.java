eac85d6de8956965eaa0676017e146affe988e14
littleyang <littleyangqh@gmail.com>
2017-01-18 16:29:45 +0800

    add exceptions

---
+++ common-utils/src/main/java/com/micro/service/exception/DaoExceptions.java
@@ -0,0 +1,11 @@
+package com.micro.service.exception;
+
+/**
+ * @description:
+ * @author: yang.zhou
+ * @version: 1.0.0
+ * @since: 2017-01-18 4:28 PM
+ */
+
+public class DaoExceptions extends Exception{
+}
