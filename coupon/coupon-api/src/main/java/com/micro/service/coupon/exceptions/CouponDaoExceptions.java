158f22eca4561bc800ce9b7b190f608658ddccad
littleyang <littleyangqh@gmail.com>
2017-01-18 16:33:34 +0800

    add exceptions

---
+++ coupon/coupon-api/src/main/java/com/micro/service/coupon/exceptions/CouponDaoExceptions.java
@@ -0,0 +1,11 @@
+package com.micro.service.coupon.exceptions;
+
+/**
+ * @description:
+ * @author: yang.zhou
+ * @version: 1.0.0
+ * @since: 2017-01-18 4:30 PM
+ */
+
+public class CouponDaoExceptions{
+}
